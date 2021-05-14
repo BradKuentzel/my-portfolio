9ca5ca278865bdafaf19be5ba192ebdf01de9bfa
Brad <Bradkuentzel@icloud.com>
2021-05-14 11:45:37 -0400

    responsiveness test

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
 			<img
 				src={image}
 				alt="retrowave synth background"
-				className="absolute object-cover w-full h-full"
+				className="absolute object-cover sm:object-scale-down w-full h-full"
 			/>
 			<section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
 				<h1 className="text-6xl text-blue-300 font-bold cursive leading-none lg:leading-snug home-name">
